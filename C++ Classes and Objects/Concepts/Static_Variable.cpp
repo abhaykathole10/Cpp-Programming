182089a5d85b7eff59a7b14b4ee26344f2ed1584
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-08 19:36:02 +0530

    Static Variables

---
+++ C++ Classes and Objects/Concepts/Static_Variable.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class myClass{
+    int a;
+    static int count;
+    public:
+    void display(){
+        cout << count << endl;
+    }
+    void fun(){
+        count++;
+    }
+};
+//Type and Scope of Static variables need to declared outside class
+int myClass :: count;
+
+int main(){
+    myClass o1,o2,o3;
+
+    o1.display();
+    o1.fun();
+    o1.display();
+    o2.fun();
+    o1.display();
+    o3.fun();
+    o1.display();
+
+    return 0;
+}
+/*
+Same variable is shared among all the obj
+Even thiugh the function calls are independent, count is same for all
+So count gets incremented 3 times
+*/
