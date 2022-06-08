255d44603ed40630b528117df5912ca802120c55
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-08 21:53:26 +0530

    Static member function

---
+++ C++ Classes and Objects/Concepts/Static_member_function.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class item{
+    static int count;
+    int code;
+    public:
+    void setCode(){
+        code = ++count;
+    }
+    void showCode(){
+        cout << "\nObject number: " << code;
+    }
+    //Static func only has access to static variables of same class
+    static void showCount(){
+        cout << "\nCount: " << count;
+    }
+};
+int item :: count;
+
+int main(){
+    item a, b;
+    a.setCode();
+    b.setCode();
+
+    //Static functions can be called directly using class name without craeting object
+    item :: showCount();
+
+    item c;
+    c.setCode();
+
+    item :: showCount();
+
+    a.showCode();
+    b.showCode();
+    c.showCode();
+
+    return 0;
+}
