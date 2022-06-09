b76996d01505c628ea041a3f3dcadbdd69f3497c
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-09 12:19:07 +0530

    Constructor calling methods

--- C++ Classes and Objects/Classes and Objects/2_Constructor.cpp
+++ C++ Classes and Objects/Classes and Objects/2_Constructor.cpp
@@ -9,18 +9,26 @@ class student{
     public:
     //Constructor doesn't have return type
     //Constructor has same name as class
-    student(string n, int r){
-        name = n;
-        rollno = r;
-        cout << "\n" << name << "'s roll no is " << rollno;
-    }
+    student(string a, int b);
 };
 
+student :: student(string n, int r){
+        name = n;
+        rollno = r;
+        cout << name << "'s roll no is " << rollno << endl;
+    }
+
 int main(){
-    //Constructor gets automatically called
+    //Constructor calling methods
+    //1. Calling constructor implicitly (Constructor gets automatically called)
     student first("Abhay", 901);
     student second("Dikshant", 905);
     student third("Rushikesh", 921);
+
+    //2. Calling constructor explicitly (Passing arguments to the function 'student')
+    student fourth = student("Cristiano", 925);
+    student fifth = student("Lionel", 933);
+    student sixth = student("Karim", 947);
 
     return 0;
 }
