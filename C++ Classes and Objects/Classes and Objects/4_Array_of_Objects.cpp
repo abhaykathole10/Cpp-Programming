91d882b0981d228cbf3aed76af29be9d8c3b73d9
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-09 15:10:06 +0530

    Array of Objects in C++ OOPs

--- C++ Classes and Objects/Classes and Objects/4_Array_of_Objects.cpp
+++ C++ Classes and Objects/Classes and Objects/4_Array_of_Objects.cpp
@@ -4,11 +4,14 @@ using namespace std;
 class employee{
     char name[10];
     int age;
+
     public:
-    void getdata();
-    void display();
+        //Function declaration
+        void getdata();
+        void display();
 };
 
+//Function definition
 void employee :: getdata(){
     cout << "Enter name: ";
     cin >> name;
@@ -19,16 +22,20 @@ void employee :: getdata(){
 void employee :: display(){
     cout << name << " is " << age << " years old\n";
 }
+
 int SIZE = 3;
 int main(){
     //Creating Object Array of "employee" class
     employee manager[SIZE];
 
     //obj.func() == manager[i].getData()
+
     //Accessing ith object
     for(int i=0; i<SIZE; i++){
         manager[i].getdata();
     }
+
+    //Displaying ith object
     for(int i=0; i<SIZE; i++){
         manager[i].display();
     }
