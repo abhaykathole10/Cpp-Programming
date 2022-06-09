f4e01078465805c9e0b9ef88236623777311025f
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-09 15:08:37 +0530

    Class and Object in C++

--- C++ Classes and Objects/Classes and Objects/1_Class_Object.cpp
+++ C++ Classes and Objects/Classes and Objects/1_Class_Object.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 class myClass{
+
     //The private(default) members of the class
     //Attributes or Data Members
     int var1;
     int var2;
+
     //Explicitily making public
     public:
+
     //func() can be accessed my the object
     //Methods or Member function
     void func(){
@@ -16,10 +19,13 @@ class myClass{
 };
 
 int main(){
+
     //Creating an object of class "myClass"
     myClass myObj;
+
     // myObj.var1 = 10; is not possible because var1 is private
     //Calling func() which is public
     myObj.func();
+
     return 0;
 }
