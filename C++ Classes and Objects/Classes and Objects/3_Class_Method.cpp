c1ad88aafc1ec7438d1ad59cbc180eb79a869159
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-09 15:09:40 +0530

    Member functions (Methods)

--- C++ Classes and Objects/Classes and Objects/3_Class_Method.cpp
+++ C++ Classes and Objects/Classes and Objects/3_Class_Method.cpp
@@ -6,7 +6,8 @@ class myClass{
     //Method declared inside the class
     void myMethod();
 };
-//Method is declared outside the class using scope resolution operator(::)
+
+//Method is defined outside the class using scope resolution operator(::)
 void myClass :: myMethod(){
     cout << "Function Declared outside the class\n";
 }
