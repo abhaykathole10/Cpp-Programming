0600e21a426d5a8db6dded8b88075e257a03499f
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-09 15:12:14 +0530

    Static Member function in C++

--- C++ Classes and Objects/Concepts/Static_member_function.cpp
+++ C++ Classes and Objects/Concepts/Static_member_function.cpp
@@ -5,17 +5,19 @@ class item{
     static int count;
     int code;
     public:
-    void setCode(){
-        code = ++count;
-    }
-    void showCode(){
-        cout << "\nObject number: " << code;
-    }
-    //Static func only has access to static variables of same class
-    static void showCount(){
-        cout << "\nCount: " << count;
-    }
+        void setCode(){
+            code = ++count;
+        }
+        void showCode(){
+            cout << "\nObject number: " << code;
+        }
+        //Static func only has access to static variables of same class
+        static void showCount(){
+            cout << "\nCount: " << count;
+        }
 };
+
+//Type and Scope of Static variables need to declared outside class
 int item :: count;
 
 int main(){
@@ -23,7 +25,7 @@ int main(){
     a.setCode();
     b.setCode();
 
-    //Static functions can be called directly using class name without craeting object
+    //Static functions can be called directly using class name without creating object
     item :: showCount();
 
     item c;
