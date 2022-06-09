6b8c79d13482333f7a173bc1c85ae2cbd32623e6
abhaykathole10 <abhaykathole10@gmail.com>
2022-06-09 15:30:39 +0530

    Array using "new" opeartor

---
+++ C++ DMA/2-1_array_using_new.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    int *p;
+    //We have declared a dynamic array with size=3
+    p = new int[3];
+
+    //Here we are taking elements
+    for(int i=0; i<5; i++){
+        cout << "Enter value: ";
+        cin >> *(p+i);
+    }
+    //The first 3 elements get the contiguous memory
+    //The last 2 gets random memory in the stack
+    for(int i=0; i<5; i++){
+        cout << endl << i << " " << (p+i) << "-> " << *(p+i);
+    }
+
+    //releasing the memory for future use
+    delete []p;
+
+    getch();
+}
